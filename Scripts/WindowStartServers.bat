b2c3cd04557b4c32dcac06162bb8ac386a74966c
Anfifo <afonseca-1995@hotmail.com>
2018-12-11 10:19:44 +0000

    fixed script

--- Scripts/WindowStartServers.bat
+++ Scripts/WindowStartServers.bat
@@ -3,11 +3,11 @@ set "filesystem=%cd%\FileSystems"
 set "auth=%cd%\AuthServer"
 
 rem  -- installs project
-cd ..
-mvn clean install -DskipTests
 
+REM start cmd /c "title INSTALLING & cd .. & mvn clean install"
+REM timeout /t 15
 rem -- Start Auth Server
-start cmd /k "title AuthServer & cd %auth% & mvn spring-boot:run"
+start cmd /c "title AuthServer & cd .. & cd AuthServer & mvn spring-boot:run"
 
 rem -- Start FileSystem
-start cmd /c "title FileSystem cd %filesystem% & mvn spring-boot:run"
+start cmd /c "title FileSystem & cd .. & cd FileSystem & mvn spring-boot:run"
