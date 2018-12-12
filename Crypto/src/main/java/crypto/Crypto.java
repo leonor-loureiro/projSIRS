d3e6fe60a7e853e9a4b3e608271879c4cf80ad19
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2018-12-12 01:54:51 +0000

    jdke

--- Crypto/src/main/java/crypto/Crypto.java
+++ Crypto/src/main/java/crypto/Crypto.java
@@ -585,8 +585,6 @@ public class Crypto {
      */
     public static boolean validateMAC(byte[] key, byte[] message, String mac, String alg){
 
-
-
         String computedMac = null;
         try {
             computedMac = computeMAC(key, message, alg);
