2cbffabbd25609d9460bee113c2009ef0228df66
leonor-loureiro <leonor.filipa@tecnico.ulisboa.pt>
2018-12-12 01:37:32 +0000

    Test authserver

--- AuthServer/src/test/java/AuthServiceTests.java
+++ AuthServer/src/test/java/AuthServiceTests.java
@@ -1,6 +1,6 @@
 import authserver.AuthService;
-import authserver.data.User;
 import authserver.db.DBConnection;
+import authserver.exception.InvalidUserException;
 import authserver.exception.UserAlreadyExistsException;
 import crypto.Crypto;
 import org.junit.After;
@@ -9,7 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
@@ -18,7 +17,7 @@ import java.util.Properties;
 public class AuthServiceTests {
 
     String username = "bjenbdwirnd82482su";
-    String saltedHash = "saltedPassword";
+    String password = "password";
     String publicKey = "publicKey";
     DBConnection db;
 
@@ -40,19 +39,112 @@ public class AuthServiceTests {
         authService = AuthService.getInstance();
     }
 
-
+    /**
+     * Successfully registers a user
+     */
     @Test
     public void successRegister() throws UserAlreadyExistsException, SQLException {
-            authService.register(username, saltedHash, publicKey);
+            authService.register(username, password, publicKey);
             Assert.assertFalse(db.getUser(username) == null);
     }
 
+    /**
+     * Tries to register a user with the same username as another that already exists
+     */
     @Test(expected = UserAlreadyExistsException.class)
     public void userAlreadyExists() throws UserAlreadyExistsException {
-        authService.register(username, saltedHash, publicKey);
-        authService.register(username, saltedHash, publicKey);
+        authService.register(username, password, publicKey);
+        authService.register(username, password, publicKey);
     }
 
+    /**
+     * Tries to register a user with invalid username format
+     */
+    @Test
+    public void invalidUsernameRegister() throws UserAlreadyExistsException {
+        String res = authService.register("jcei!.*", password, publicKey);
+        Assert.assertTrue(res == null);
+    }
+
+    /**
+     * Successful login
+     */
+    @Test
+    public void successLogin() throws UserAlreadyExistsException, InvalidUserException {
+        authService.register(username, password, publicKey);
+        String res = authService.login(username, password);
+
+        Assert.assertTrue(res != null);
+    }
+
+    /**
+     * Tries to login a user that doesn't exist
+     */
+    @Test(expected = InvalidUserException.class)
+    public void userDoesNotExist() throws InvalidUserException {
+        authService.login(username, password);
+    }
+
+    /**
+     * Tries to login a user with invalid username format
+     */
+    @Test
+    public void invalidUsernameLogin() throws InvalidUserException, UserAlreadyExistsException {
+        authService.register(username, password, publicKey);
+        String res = authService.login("ie.*!", password);
+
+        Assert.assertTrue(res == null);
+    }
+
+    /**
+     * Successfully authenticates a user
+     */
+    @Test
+    public void successAuthenticate() throws UserAlreadyExistsException {
+        String token = authService.register(username, password, publicKey);
+        Assert.assertTrue(authService.authenticate(username, token));
+
+    }
+
+    /**
+     * Tries to authenticate a user with an invalid username format
+     */
+    @Test
+    public void invalidUsernameAuthenticate() throws UserAlreadyExistsException {
+        String token = authService.register(username, password, publicKey);
+        Assert.assertFalse(authService.authenticate("nue*!ís", token));
+
+    }
+
+
+
+    /**
+     * Successfully retrieves a user's public key
+     */
+    @Test
+    public void successfulGetPublicKey() throws UserAlreadyExistsException, InvalidUserException {
+        authService.register(username, password, publicKey);
+        String extractedKey = authService.getPublicKey(username);
+
+        Assert.assertEquals(publicKey, extractedKey);
+    }
+
+    /**
+     * Tries to retrieve the key of a user that does not exist
+     */
+    @Test(expected = InvalidUserException.class)
+    public void getPublicKeyUserNotExists() throws InvalidUserException {
+        authService.getPublicKey(username);
+    }
+
+    /**
+     * Tries to get the public key for a username that is in an invalid format
+     */
+    @Test
+    public void getPublicKeyInvalidUsername() throws InvalidUserException, UserAlreadyExistsException {
+        authService.register(username, password, publicKey);
+        Assert.assertTrue(authService.getPublicKey("od*!d") == null);
+    }
 
 
     @After
